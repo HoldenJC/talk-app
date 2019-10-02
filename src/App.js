2d107ed77ea812d0d35d3f149b7149152788bc00
Holden Clark <holdenj_c@yahoo.com>
2019-10-02 13:46:56 -0700

    add react-router button to screen links, basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
+
+// components
+import Navbar from './components/Navbar'
+
+// screens
+import home from './screens/home'
+import login from './screens/login'
+import signup from './screens/signup'
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>Talk App</h1>
+				<Router>
+					<Navbar />
+					<div className="container">
+						<Switch>
+							<Route exact path="/" component={home} />
+							<Route exact path="/login" component={login} />
+							<Route exact path="/signup" component={signup} />
+						</Switch>
+					</div>
+				</Router>
 			</div>
 		)
 	}
