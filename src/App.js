ad1149d11adb1454915328f4fb5966c8eef71693
Holden Clark <holdenj_c@yahoo.com>
2019-10-02 16:52:31 -0700

    add Talk component to render talks on homepage, images folder to hold logos, add additional styling, add login form that redirects to home on successful login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 
 // components
 import Navbar from './components/Navbar'
@@ -10,21 +12,43 @@ import home from './screens/home'
 import login from './screens/login'
 import signup from './screens/signup'
 
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			light: '#757de8',
+			main: '#3F51B5',
+			dark: '#002984',
+			contrastText: '#fff'
+		},
+		secondary: {
+			light: '#ffb04c',
+			main: '#F57F17',
+			dark: '#bc5100',
+			contrastText: '#000'
+		}
+	},
+	typography: {
+		useNextVariants: true
+	}
+})
+
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<Router>
-					<Navbar />
-					<div className="container">
-						<Switch>
-							<Route exact path="/" component={home} />
-							<Route exact path="/login" component={login} />
-							<Route exact path="/signup" component={signup} />
-						</Switch>
-					</div>
-				</Router>
-			</div>
+			<MuiThemeProvider theme={theme}>
+				<div className="App">
+					<Router>
+						<Navbar />
+						<div className="container">
+							<Switch>
+								<Route exact path="/" component={home} />
+								<Route exact path="/login" component={login} />
+								<Route exact path="/signup" component={signup} />
+							</Switch>
+						</div>
+					</Router>
+				</div>
+			</MuiThemeProvider>
 		)
 	}
 }
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import Grid from '@material-ui/core/Grid'
+
+import Talk from '../components/Talk'
 
 class home extends Component {
+	state = {
+		talks: null
+	}
+
+	componentDidMount() {
+		axios
+			.get('/talks')
+			.then((res) => {
+				this.setState({
+					talks: res.data
+				})
+			})
+			.catch((err) => console.log(err))
+	}
 	render() {
+		let recentTalksMarkup = this.state.talks ? (
+			this.state.talks.map((talk) => <Talk key={talk.talkId} talk={talk} />)
+		) : (
+			<p>Loading...</p>
+		)
 		return (
-			<div>
-				<h1>Home Page</h1>
-			</div>
+			<Grid container spacing={16}>
+				<Grid item sm={8} xs={12}>
+					{recentTalksMarkup}
+				</Grid>
+				<Grid item sm={4} xs={12}>
+					<p>Profile...</p>
+				</Grid>
+			</Grid>
 		)
 	}
 }
