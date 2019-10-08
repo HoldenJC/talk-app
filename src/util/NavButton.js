4f02656aaadfd92616bd141dced2e39519855f1e
Holden Clark <holdenj_c@yahoo.com>
2019-10-08 12:04:09 -0700

    hide tip popup and ripple effect from comment icon

--- src/util/NavButton.js
+++ src/util/NavButton.js
@@ -4,8 +4,21 @@ import IconButton from '@material-ui/core/IconButton'
 
 export default ({ children, tip, onClick, btnClassName, tipClassName, placement }) => (
 	<Tooltip title={tip} className={tipClassName} placement={placement}>
-		<IconButton onClick={onClick} className={btnClassName}>
-			{children}
-		</IconButton>
+		{tip === 'comments' ? (
+			<IconButton
+				onClick={onClick}
+				className={btnClassName}
+				disabled
+				disableFocusRipple
+				disableRipple
+				disableTouchRipple
+			>
+				{children}
+			</IconButton>
+		) : (
+			<IconButton onClick={onClick} className={btnClassName}>
+				{children}
+			</IconButton>
+		)}
 	</Tooltip>
 )
